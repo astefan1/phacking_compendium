d57fa79ac3912923b0b32435b996704b08b98d81
Angelika Stefan <anmaste92@gmail.com>
2022-11-11 15:10:55 +0100

    add regression to strategy 2: multiple IVs

--- phackR/R/selectiveReportingIV.R
+++ phackR/R/selectiveReportingIV.R
@@ -4,30 +4,32 @@
 
 #' Simulate dataset with multiple independent variables
 #' @description Outputs data frame with multiple independent variables
-#' @param nobs.group Scalar defining number of observations per group
+#' @param nobs.group Scalar defining number of observations per group (or number of observations in predictors in regression)
 #' @param nvar Number of independent variables in the data frame
 #' @param r Desired correlation between the independent variables (scalar)
 
-.sim.multIV <- function(nobs.group, nvar, r){
+.sim.multIV <- function(nobs.group, nvar, r, regression = FALSE){
 
   # Observations per group
   if(length(nobs.group) == 1) nobs.group <- rep(nobs.group, 2)
 
-  # Simulate control group
+  # Simulate control group / criterion variable
   control <- rnorm(nobs.group[1])
+  if(regression) criterion <- control
 
-  # Simulate multiple experimental groups
+  # Simulate multiple experimental groups / predictor variables
   ivs <- .sim.multcor(nobs = nobs.group[2], nvar = nvar, r = r)
 
   # Generate data frame
   res <- cbind(control, ivs)
+  if(regression) colnames(res)[1] <- "criterion"
 
   return(res)
 
 }
 
-#' P-Hacking function for multiple independent variables
-#' @description Outputs a p-hacked p-value and a vector of all p-values that were computed in the process
+#' P-Hacking function for multiple independent variables in a t-test
+#' @description Returns a p-hacked p-value and a vector of all p-values that were computed in the process
 #' @param df Data frame (wide format) containing a control group variable and multiple treatment group variables
 #' @param ivs Location of the independent variables (treatment groups) in the (wide) data frame
 #' @param control Location of the control group in the (wide) data frame
@@ -36,7 +38,7 @@
 #' @param alpha Significance level of the t-test (default: 0.05)
 #' @importFrom stats t.test
 
-.multIVhack <- function(df, ivs, control, strategy = "firstsig", alternative = "two.sided", alpha = 0.05){
+.multIVhack_ttest <- function(df, ivs, control, strategy = "firstsig", alternative = "two.sided", alpha = 0.05){
 
   treatm <- df[, ivs]
   control <- df[, control]
@@ -68,6 +70,43 @@
 
 }
 
+#' P-Hacking function for multiple predictors in a regression
+#' @description Returns a p-hacked p-value and a vector of all p-values that were computed in the process
+#' @param df Data frame containing a criterion variable and multiple predictor variables
+#' @param ivs Location of the independent variables (predictors) in the data frame
+#' @param control Location of the criterion in the data frame
+#' @param strategy String value: One out of "firstsig", "smallest", "smallest.sig"
+#' @param alternative Direction of the t-test ("two.sided", "less", "greater")
+#' @param alpha Significance level of the t-test (default: 0.05)
+#' @importFrom stats t.test
+
+.multIVhack_reg <- function(df, ivs, control, strategy = "firstsig", alternative="two.sided", alpha = 0.05){
+  
+  predictors <- df[, ivs]
+  criterion <- df[, control]
+  
+  # Prepare dataset
+  ps <- rep(NA, length(ivs))
+  r2s <- rep(NA, length(ivs))
+  
+  # Compute regressions
+  for(i in 1:length(ivs)){
+    mod <- summary(stats::lm(criterion ~ predictors[,i]))
+    ps[i] <- mod$coefficients[2, 4]
+    r2s[i] <- mod$r.squared
+  }
+  
+  # Select final p-hacked p-value based on strategy
+  p.final <- .selectpvalue(ps = ps, strategy = strategy, alpha = alpha)
+  r2.final <- r2s[ps == p.final]
+
+  return(list(p.final = p.final,
+              ps = ps,
+              r2.final = r2.final,
+              r2s = r2s))
+  
+}
+
 #' Simulate p-Hacking with multiple independent variables
 #' @description Outputs a matrix containing the p-hacked p-values (\code{ps.hack}) and the original p-values (\code{ps.orig}) from all iterations
 #' @param nobs.group Vector giving number of observations per group
@@ -80,16 +119,17 @@
 #' @param shinyEnv Is the function run in a Shiny session? TRUE/FALSE
 #' @export
 
-sim.multIVhack <- function(nobs.group, nvar, r, strategy = "firstsig", iter = 1000, alternative = "two.sided", alpha = 0.05, shinyEnv = FALSE){
+sim.multIVhack <- function(nobs.group, nvar, r, regression=FALSE, strategy = "firstsig", iter = 1000, alternative = "two.sided", alpha = 0.05, shinyEnv = FALSE){
 
   # Simulate as many datasets as desired iterations
   dat <- list()
   for(i in 1:iter){
-    dat[[i]] <- .sim.multIV(nobs.group = nobs.group, nvar = nvar, r = r)
+    dat[[i]] <- .sim.multIV(nobs.group = nobs.group, nvar = nvar, r = r, regression=regression)
   }
 
   # Apply p-hacking procedure to each dataset
-
+  .multIVhack <- ifelse(regression, .multIVhack_reg, .multIVhack_ttest)
+  
   .multIVhacklist <- function(x){
     .multIVhack(df = x, ivs = c(2:(nvar+1)), control = 1,
                 strategy = strategy, alternative = alternative, alpha = alpha)
@@ -123,11 +163,14 @@ sim.multIVhack <- function(nobs.group, nvar, r, strategy = "firstsig", iter = 10
     ps.orig[i] <- res[[i]][["ps"]][1]
     r2s.hack[i] <- res[[i]][["r2.final"]]
     r2s.orig[i] <- res[[i]][["r2s"]][1]
-    ds.hack[i] <- res[[i]][["d.final"]]
-    ds.orig[i] <- res[[i]][["ds"]][1]
+    if(regression){
+      ds.hack[i] <- res[[i]][["d.final"]]
+      ds.orig[i] <- res[[i]][["ds"]][1]
+    }
   }
 
-  res <- cbind(ps.hack, ps.orig, r2s.hack, r2s.orig, ds.hack, ds.orig)
+  res <- cbind(ps.hack, ps.orig, r2s.hack, r2s.orig)
+  if(!regression) res <- cbind(res, ds.hack, ds.orig)
 
   return(res)
 
--- simulations/multIVHack_simulation.R
+++ simulations/multIVHack_simulation.R
@@ -8,30 +8,62 @@ r <- c(0, 0.3, 0.8)
 
 cond.multIVhack <- expand.grid(nobs.group, nvar, r)
 
-#### Simulation ####
-simresults.multIVhack <- list()
+#### Simulation t-Test ####
+simresults.multIVhack_ttest <- list()
 
-simmultiple.multIVhack <- function(par, strategy){
+simmultiple.multIVhack_ttest <- function(par, strategy){
   data.frame(sim.multIVhack(nobs.group = par[1],
                             nvar = par[2],
                             r = par[3],
+                            regression = FALSE,
                             strategy = strategy,
-                            iter = 1000,
+                            iter = 10000,
                             alternative = "two.sided",
                             alpha = 0.05))
 }
 
-simresults.multIVhack$firstsig <- apply(cond.multIVhack, 1, function(x) {
-  simmultiple.multIVhack(x, strategy = "firstsig")
+simresults.multIVhack_ttest$firstsig <- apply(cond.multIVhack, 1, function(x) {
+  simmultiple.multIVhack_ttest(x, strategy = "firstsig")
 })
 
-simresults.multIVhack$smallest <- apply(cond.multIVhack, 1, function(x) {
-  simmultiple.multIVhack(x, strategy = "smallest")
+simresults.multIVhack_ttest$smallest <- apply(cond.multIVhack, 1, function(x) {
+  simmultiple.multIVhack_ttest(x, strategy = "smallest")
 })
 
-simresults.multIVhack$smallestsig <- apply(cond.multIVhack, 1, function(x) {
-  simmultiple.multIVhack(x, strategy = "smallest.sig")
+simresults.multIVhack_ttest$smallestsig <- apply(cond.multIVhack, 1, function(x) {
+  simmultiple.multIVhack_ttest(x, strategy = "smallest.sig")
 })
 
-save(simresults.multIVhack, file = "simulations/SIM_multIVhack.RData")
+save(simresults.multIVhack_ttest, file = "simulations/SIM_multIVhack_ttest.RData")
+
+### Simulation regression ####
+
+simresults.multIVhack_reg <- list()
+
+simmultiple.multIVhack_reg <- function(par, strategy){
+  data.frame(sim.multIVhack(nobs.group = par[1],
+                            nvar = par[2],
+                            r = par[3],
+                            regression = TRUE,
+                            strategy = strategy,
+                            iter = 10000,
+                            alternative = "two.sided",
+                            alpha = 0.05))
+}
+
+simresults.multIVhack_reg$firstsig <- apply(cond.multIVhack, 1, function(x) {
+  simmultiple.multIVhack_reg(x, strategy = "firstsig")
+})
+
+simresults.multIVhack_reg$smallest <- apply(cond.multIVhack, 1, function(x) {
+  simmultiple.multIVhack_reg(x, strategy = "smallest")
+})
+
+simresults.multIVhack_reg$smallestsig <- apply(cond.multIVhack, 1, function(x) {
+  simmultiple.multIVhack_reg(x, strategy = "smallest.sig")
+})
+
+save(simresults.multIVhack_reg, file = "simulations/SIM_multIVhack_reg.RData")
+
+
 
