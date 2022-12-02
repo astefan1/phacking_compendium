98556300943012d54c3d1dc9f7d4ad06418ed9ab
Angelika Stefan <anmaste92@gmail.com>
2022-12-03 00:23:55 +0100

    update BFdist

--- simulations/plot_BFdist.R
+++ simulations/plot_BFdist.R
@@ -10,6 +10,7 @@ lapply(paste0("simulations/",
 
 library(ggplot2)
 library(dplyr)
+library(BayesFactor)
 
 # Conditions for multiple dependent variables
 
@@ -19,59 +20,92 @@ r <- c(0, 0.3, 0.8)                 # correlation between dependent variables
 
 cond.multDVhack <- expand.grid(nobs.group, nvar, r)
 
-# Select conditions to show in the plot
+# ------------------------------------------------------------------------------
+# Extract p-values and t-values and compute BFs ####
+# ------------------------------------------------------------------------------
 
-nobs.extracted <- 300 # to get plots for N=100, change this to 100
-cond.plot <- which(cond.multDVhack$Var1 == nobs.extracted & cond.multDVhack$Var3 == 0)
+getBFs <- function(simdat, conddat, nobs, r, strategy){
+  
+  cond.plot <- which(conddat$Var1 == nobs & conddat$Var3 == r)
+  
+  # extract p-values and t-values
+  ps.hack <- unlist(lapply(simdat[[strategy]][cond.plot], function(x) x$ps.hack))
+  ts.hack <- qt(ps.hack/2, df=nobs-2)
+  ps.nohack <- unlist(lapply(simdat[[strategy]][cond.plot], function(x) x$ps.orig))
+  ts.nohack <- qt(ps.nohack/2, df=nobs-2)
+  
+  # compute BFs
+  BFs.hack <- lapply(ts.hack, function(x) ttest.tstat(x, n1=nobs, n2=nobs, simple=TRUE))
+  BFs.hack <- unname(simplify2array(BFs.hack))
+  BFs.nohack <- lapply(ts.nohack, function(x) ttest.tstat(x, n1=nobs, n2=nobs, simple=TRUE))
+  BFs.nohack <- unname(simplify2array(BFs.nohack))
+  
+  return(cbind(BFs.hack, BFs.nohack))
+}
 
-# Extract t-values and compute BFs
-# To get plots for other reporting strategies, exchange all firstsig by smallestsig or smallest
+# Compute all BFs
 
-ps.hack <- unlist(lapply(simresults.multDVhack$firstsig[cond.plot], function(x) x$ps.hack))
-ts.hack <- qt(ps.hack/2, df=nobs.extracted-2)
-ps.nohack <- unlist(lapply(simresults.multDVhack$firstsig[cond.plot], function(x) x$ps.orig))
-ts.nohack <- qt(ps.nohack/2, df=nobs.extracted-2)
-library(BayesFactor)
-BFs.hack <- lapply(ts.hack, function(x) ttest.tstat(x, n1=nobs.extracted, n2=nobs.extracted, simple=TRUE))
-BFs.hack <- unname(simplify2array(BFs.hack))
-BFs.nohack <- lapply(ts.nohack, function(x) ttest.tstat(x, n1=nobs.extracted, n2=nobs.extracted, simple=TRUE))
-BFs.nohack <- unname(simplify2array(BFs.nohack))
+BF50_firstsig <- getBFs(simdat=simresults.multDVhack, conddat=cond.multDVhack, nobs=50, r=0, strategy="firstsig")
+BF50_smallestsig <- getBFs(simdat=simresults.multDVhack, conddat=cond.multDVhack, nobs=50, r=0, strategy="smallestsig")
+BF50_smallest <- getBFs(simdat=simresults.multDVhack, conddat=cond.multDVhack, nobs=50, r=0, strategy="smallest")
+
+BF300_firstsig <- getBFs(simdat=simresults.multDVhack, conddat=cond.multDVhack, nobs=300, r=0, strategy="firstsig")
+BF300_smallestsig <- getBFs(simdat=simresults.multDVhack, conddat=cond.multDVhack, nobs=300, r=0, strategy="smallestsig")
+BF300_smallest <- getBFs(simdat=simresults.multDVhack, conddat=cond.multDVhack, nobs=300, r=0, strategy="smallest")
+
+
+# ------------------------------------------------------------------------------
+# Plot the legend ####
+# ------------------------------------------------------------------------------
+
 
 # Data for plot
 
-iter <- nrow(simresults.multDVhack$firstsig[[1]])
-nDV <- rep(cond.multDVhack[cond.plot,]$Var2, each=iter)
+plotBFDist <- function(BFobj, conddat, nobs, r){
+  
+  cond.plot <- which(conddat$Var1 == nobs & conddat$Var3 == r)
+  iter <- nrow(BFobj)
+  nDV <- rep(conddat[cond.plot,]$Var2, each=iter)
+  BFs.hack <- BFobj[,1]
+  BFs.nohack <- BFobj[,2]
+  
+  plotdat <- data.frame(lbfs.hack=log(BFs.hack),
+                        lbfs.nohack=log(BFs.nohack),
+                        nDV=as.factor(nDV))
+  
+  newplotdat <- plotdat %>% # compute densities from ES
+    group_by(nDV) %>%
+    do(data.frame(loc.bfs = density(.$lbfs.hack)$x,
+                  dens.bfs = density(.$lbfs.hack)$y / (2.2*max(density(.$lbfs.hack)$y)),
+                  loc.bfs.nohack = density(.$lbfs.nohack)$x,
+                  dens.bfs.nohack = -1*density(.$lbfs.nohack)$y/(2*max(density(.$lbfs.nohack)$y))))
+  newplotdat$dens.bfs <- newplotdat$dens.bfs + as.numeric(newplotdat$nDV) # y-offset for different number of DVs
+  newplotdat$dens.bfs.nohack <- newplotdat$dens.bfs.nohack + as.numeric(newplotdat$nDV)
+  
+  newplotdat$title <- paste0("N = ", nobs)
+  
+  ggplot(data=newplotdat, aes(group = nDV)) +
+    geom_polygon(aes(y=dens.bfs, x=loc.bfs), fill = "#FFAE4A") +
+    geom_polygon(aes(y=dens.bfs.nohack, x=loc.bfs.nohack), fill = "#5AB4BD") +
+    labs(x = "Bayes factor",
+         y = "Number of dependent variables") +
+    theme_bw() +
+    theme(text = element_text(size=35),
+          axis.title = element_text(size=25),
+          axis.text = element_text(size=25)) +
+    scale_y_continuous(breaks = c(1,2,3), labels = c("3", "5", "10")) +
+    scale_x_continuous(breaks = log(c(1/10, 1/3, 1, 3, 10)), labels = c("1/10", "1/3", "1", "3", "10")) +
+    coord_cartesian(xlim = log(c(1/20, 100))) +
+    facet_grid(. ~ title) +
+    geom_vline(xintercept = 0, linetype = "dashed", col = "grey", lwd = 1.5) 
+  
+  
+}
 
-plotdat <- data.frame(lbfs.hack=log(BFs.hack),
-                      lbfs.nohack=log(BFs.nohack),
-                      nDV=as.factor(nDV))
+plotBFDist(BFobj=BF50_firstsig, conddat=cond.multDVhack, nobs=50, r=0)
+plotBFDist(BFobj=BF50_smallestsig, conddat=cond.multDVhack, nobs=50, r=0)
+plotBFDist(BFobj=BF50_smallest, conddat=cond.multDVhack, nobs=50, r=0)
 
-newplotdat <- plotdat %>% # compute densities from ES
-  group_by(nDV) %>%
-  do(data.frame(loc.bfs = density(.$lbfs.hack)$x,
-                dens.bfs = density(.$lbfs.hack)$y / (2.2*max(density(.$lbfs.hack)$y)),
-                loc.bfs.nohack = density(.$lbfs.nohack)$x,
-                dens.bfs.nohack = -1*density(.$lbfs.nohack)$y/(2*max(density(.$lbfs.nohack)$y))))
-newplotdat$dens.bfs <- newplotdat$dens.bfs + as.numeric(newplotdat$nDV) # y-offset for different number of DVs
-newplotdat$dens.bfs.nohack <- newplotdat$dens.bfs.nohack + as.numeric(newplotdat$nDV)
-
-newplotdat$title <- paste0("N = ", nobs.extracted)
-
-ggplot(data=newplotdat, aes(group = nDV)) +
-  geom_polygon(aes(y=dens.bfs, x=loc.bfs), fill = "#FFAE4A") +
-  geom_polygon(aes(y=dens.bfs.nohack, x=loc.bfs.nohack), fill = "#5AB4BD") +
-  labs(x = "Bayes factor",
-       y = "Number of dependent variables") +
-  theme_bw() +
-  theme(text = element_text(size=35),
-        axis.title = element_text(size=25),
-        axis.text = element_text(size=25)) +
-  scale_y_continuous(breaks = c(1,2,3), labels = c("3", "5", "10")) +
-  scale_x_continuous(breaks = log(c(1/10, 1/3, 1, 3, 10)), labels = c("1/10", "1/3", "1", "3", "10")) +
-  coord_cartesian(xlim = log(c(1/20, 100))) +
-  facet_grid(. ~ title) +
-  geom_vline(xintercept = 0, linetype = "dashed", col = "grey", lwd = 1.5) 
-                     
-
-plot.new()
-legend(x = "topleft", legend = c("p-hacked", "original"), fill = c("#FFAE4A", "#5AB4BD"))
+plotBFDist(BFobj=BF300_firstsig, conddat=cond.multDVhack, nobs=300, r=0)
+plotBFDist(BFobj=BF300_smallestsig, conddat=cond.multDVhack, nobs=300, r=0)
+plotBFDist(BFobj=BF300_smallest, conddat=cond.multDVhack, nobs=300, r=0)
